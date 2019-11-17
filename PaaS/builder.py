58a6e1aac38d6a826776a835f76c5d23ac9e193b
Shubham <sbhtwr170@gmail.com>
2019-11-17 12:15:26 +0530

    build.py

---
+++ PaaS/builder.py
@@ -0,0 +1,94 @@
+import docker
+import time
+from anytree import *
+
+# init ID counter for unique ids for exchange identifiers
+# format exch<exch_cnt>s
+exch_cnt = 0
+
+# Construct a tree representing the pipeline
+
+# First is always the consumer from rmq, we need to pass a producer and a 
+root = Node("rmq_cons")
+com0 = Node("com0", parent=root)
+
+com1 = Node("com1", parent=com0)
+com2 = Node("com2", parent=com0)
+com3 = Node("com3", parent=com0)
+
+com4 = Node("com4", parent=com2)
+
+# print the tree
+print(RenderTree(root, style=DoubleStyle))
+
+# init pipeline
+
+# Get a docker client
+client = docker.from_env()
+
+# Create a global network using Linux bridge driver 
+global_net = client.networks.create("global_net", driver="bridge")
+
+# Shared folder configuration: location of the folder on host containing the scripts
+shared = {'/home/shubham/Desktop/PaaS/shared': {'bind': '/home', 'mode': 'rw'}}
+
+# Launch a pipeline consisting of three containers, mqtt[-p 1883:1883, mqtt_cons.py] -> rmq_cons.py -> docker logs
+container1 = client.containers.run("cloudassignment/rabbitmq",
+						#command = '/bin/bash',
+						detach = True,    # If kept to false, will block the script. If script is killed, container exits 
+						#devices = ['/home/shubham/Desktop/shared:/home:rwm'], # This is used for mounting devices, not folders (volmes). So its wrong. Ignore.
+						hostname = 'rmq',
+						name = 'rmq',
+						network = 'global_net',
+						privileged = True,
+						stdin_open = True, # have to keep STDIN open to avoid container exit
+						tty = True,		   # allocate pseudo tty to avoid container exit
+						volumes = shared #For ease, we will mount a common shared folder on all containers.
+					  )
+
+container2 = client.containers.run("eclipse-mosquitto",
+						#command = '/bin/bash',
+						detach = True,    # If kept to false, will block the script. If script is killed, container exits 
+						#devices = ['/home/shubham/Desktop/shared:/home:rwm'], # This is used for mounting devices, not folders (volmes). So its wrong. Ignore.
+						hostname = 'mqtt',
+						name = 'mqtt',
+						network = 'global_net',
+						ports = {'1883/tcp': 1884, '9001/tcp': 9001},
+						privileged = True,
+						stdin_open = True, # have to keep STDIN open to avoid container exit
+						tty = True,		   # allocate pseudo tty to avoid container exit
+						volumes = shared #For ease, we will mount a common shared folder on all containers.
+					  )
+
+print(container1)
+print(container2)
+print("Started rmq and mqtt. Waiting for 10 seconds before starting dependent services.")
+time.sleep(10)
+
+container3 = client.containers.run("cloudassignment/mqtt", 
+						command = ['home/mqtt_cons.py', 'mqtt','rmq','logs'],
+						detach = True,    # If kept to false, will block the script. If script is killed, container exits 
+						#devices = ['/home/shubham/Desktop/shared:/home:rwm'], # This is used for mounting devices, not folders (volmes). So its wrong. Ignore.
+						entrypoint = ['python'],
+						hostname = 'mqtt_cons',
+						name = 'mqtt_cons',
+						network = 'global_net',
+						privileged = True,
+						#publish_all_ports = True, #publish all ports to the host, so that we can send json data to it!
+						stdin_open = True, # have to keep STDIN open to avoid container exit
+						tty = True,		   # allocate pseudo tty to avoid container exit
+						volumes = shared #For ease, we will mount a common shared folder on all containers.
+					  )
+
+print(container3)
+print("Started mqtt_cons")                   
+print("Building tree...")
+
+# Iterate the tree in pre-order ways
+for node in PreOrderIter(root):
+    
+
+
+
+
+
