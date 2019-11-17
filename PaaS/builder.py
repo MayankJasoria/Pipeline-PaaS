8371dd5f1a052598bcc836937a022d0a932aa7eb
Shubham <sbhtwr170@gmail.com>
2019-11-17 13:26:54 +0530

    Implemented Tree->Pipeline

--- PaaS/builder.py
+++ PaaS/builder.py
@@ -1,10 +1,15 @@
 import docker
 import time
+import os
 from anytree import *
 
 # init ID counter for unique ids for exchange identifiers
 # format exch<exch_cnt>s
 exch_cnt = 0
+
+# init ID counter for unique ids for containers
+# format exch<exch_cnt>s
+cont_cnt = 0
 
 # Construct a tree representing the pipeline
 
@@ -30,7 +35,7 @@ client = docker.from_env()
 global_net = client.networks.create("global_net", driver="bridge")
 
 # Shared folder configuration: location of the folder on host containing the scripts
-shared = {'/home/shubham/Desktop/PaaS/shared': {'bind': '/home', 'mode': 'rw'}}
+shared = {os.getcwd() + os.path.sep + "shared": {'bind': '/home', 'mode': 'rw'}}
 
 # Launch a pipeline consisting of three containers, mqtt[-p 1883:1883, mqtt_cons.py] -> rmq_cons.py -> docker logs
 container1 = client.containers.run("cloudassignment/rabbitmq",
@@ -80,12 +85,85 @@ container3 = client.containers.run("cloudassignment/mqtt",
 						volumes = shared #For ease, we will mount a common shared folder on all containers.
 					  )
 
-print(container3)
-print("Started mqtt_cons")                   
-print("Building tree...")
+container4 = client.containers.run("cloudassignment/base", 
+						command = ['home/rmq_com.py', 'rmq','logs', 'exch'+str(exch_cnt)],
+						detach = True,    # If kept to false, will block the script. If script is killed, container exits 
+						#devices = ['/home/shubham/Desktop/shared:/home:rwm'], # This is used for mounting devices, not folders (volmes). So its wrong. Ignore.
+						entrypoint = ['python'],
+						hostname = 'cons',
+						name = 'cons',
+						network = 'global_net',
+						privileged = True,
+						#publish_all_ports = True, #publish all ports to the host, so that we can send json data to it!
+						stdin_open = True, # have to keep STDIN open to avoid container exit
+						tty = True,		   # allocate pseudo tty to avoid container exit
+						volumes = shared #For ease, we will mount a common shared folder on all containers.
+					  )                      
 
+root.container = container4
+root.exch_cons = 'logs'
+root.exch_prod = 'exch'+str(exch_cnt)
+
+print(container3)
+print(container4)
+print("Started mqtt_cons")
+print("Started rmq_cons")   
+
+print(container3)
+print(container4)
+
+print("Building tree...")
 # Iterate the tree in pre-order ways
-for node in PreOrderIter(root):
+for node in PreOrderIter(com0):
+    parent = node.parent
+    exch_cnt = exch_cnt + 1
+    node.exch_cons = parent.exch_prod
+    node.exch_prod = 'exch'+str(exch_cnt)
+    node.container = client.containers.run("cloudassignment/base", 
+						command = ['home/rmq_com.py', 'rmq', parent.exch_prod, node.exch_prod],
+						detach = True,    # If kept to false, will block the script. If script is killed, container exits 
+						#devices = ['/home/shubham/Desktop/shared:/home:rwm'], # This is used for mounting devices, not folders (volmes). So its wrong. Ignore.
+						entrypoint = ['python'],
+						hostname = node.name,
+						name = node.name,
+						network = 'global_net',
+						privileged = True,
+						#publish_all_ports = True, #publish all ports to the host, so that we can send json data to it!
+						stdin_open = True, # have to keep STDIN open to avoid container exit
+						tty = True,		   # allocate pseudo tty to avoid container exit
+						volumes = shared #For ease, we will mount a common shared folder on all containers.
+					  )
+    print(node.name)
+    print(node.container)               
+
+print("Send data stream to mqtt protocol at localhost:1884!!")
+
+input("Press Enter to shut down the platform...")
+
+print("Stopping containers...")
+# Cleanup: close all the containers and remove the network
+
+for node in PreOrderIter(com0):
+    node.container.stop()
+
+container4.stop()
+container3.stop()
+container2.stop()
+container1.stop()
+
+print("Removing containers...")
+container4.remove()
+container3.remove()
+container2.remove()
+container1.remove()
+
+for node in PreOrderIter(com0):
+    node.container.remove()
+
+print("Removing network: global_net...")
+global_net.remove()
+
+print("Cleanup complete. Exiting...")                      
     
 
 
