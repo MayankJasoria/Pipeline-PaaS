3e32f376d2dc046c7df6a3483c52d9c915f66fc3
palakvij <55702689+palakvij@users.noreply.github.com>
2019-11-22 13:38:58 +0530

    Add files via upload

--- PaaS/website.py
+++ PaaS/website.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request
 from main import BuildPipeline
+from config import Config
 
 app=Flask(__name__)
 
@@ -9,26 +10,27 @@ pipeline = None
 def index():
     return render_template("index.html")
 
-@app.route('/', methods=["POST"])
+@app.route('/', methods=["GET","POST"])
 def registerService():
     if request.form.post['action'] == "Add Service":
         # call add service
         if request.form['parent'] == "":
-            pipeline.addService(request.form['name'], 'rmq_com.py')
+            pipeline.addService(request.form['name'], config.services[request.form['services']])
+            print(config.services[request.form['services']])
         else:
-            pipeline.addService(request.form['name'], 'rmq_com.py', request.form['parent'])
+            pipeline.addService(request.form['name'], config.services[request.form['services']], request.form['parent'])
     else:# request.form.post['action'] == "Remove Service":
         # call delelteService
         pipeline.removeService(request.form['name'])
         # do nothing
-    return render_template('index.html')
+    #return render_template('index.html')
 
-@app.route('/build/', methods=["POST"])
+@app.route('/build/', methods=["GET","POST"])
 def build():
     pipeline.buildPipeline()
     return render_template("built.html")
 
-@app.route('/pipeline/', methods=["POST"])
+@app.route('/pipeline/', methods=["GET","POST"])
 def terminate():
     pipeline.terminatePipeline()
     return render_template("index.html")
